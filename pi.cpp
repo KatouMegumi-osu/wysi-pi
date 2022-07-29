139656b59c4a9d87b7ccdc3b38c38358e588833e
KatouMegumi-osu <fffuuunnb@gmail.com>
2022-07-29 08:52:34 +0500

    Add files via upload

---
+++ pi.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+//Attempt to get pi using inifnite Leibniz formula
+int main()
+{
+    constexpr double division = 1;
+    double divided = 1;
+
+     counter = 0;
+    double quarter_pi = 1;
+
+    while(counter < 2147483647) {
+        ++counter;
+        quarter_pi = quarter_pi-(division/(divided+=2))+(division/(divided+=2));
+
+        //Kudos to Zhuriel#0110 on thepoon discord
+        if (counter % 100000000 == 0) {
+            cout << "Checkpoint [" << counter << "]: " << quarter_pi*4 << '\n';
+            cout.precision(90);
+        } 
+
+        if (counter == 2147483647) {
+            cout << "last pi is: " << quarter_pi*4 << '\n';
+            cout.precision(90);
+        } 
+    }
+}
